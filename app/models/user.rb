d8c47646b4f29d7bf4052822d4ff80247c3848b8
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-10-03 00:14:05 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :comments
+  has_many :buys
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+
+  with_options presence: true, format: { with:/\A[ァ-ヶー－]+\z/, message:"" } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  with_options presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]+\z/, message:"" } do
+    validates :first_name
+    validates :last_name
+  end
+
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6}\z/
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX, message:"" }
 end
