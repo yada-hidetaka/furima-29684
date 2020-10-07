6fabe02c42ef301cef4e0e5fb99810222c1df8b9
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-10-07 19:00:45 +0900

    バリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,30 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :status
+  belongs_to_active_hash :category
+  belongs_to_active_hash :ship_form
+  belongs_to_active_hash :day
+
+  has_one_attached :image
+  belongs_to :user
+  has_many :comments
+  has_one :buy
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :status_id
+    validates :prefecture
+    validates :category_id
+    validates :image
+    validates :day_id
+    validates :ship_form_id
+  end
+
+  validates :category_id, :day_id, :ship_form_id, :prefecture_id, :status_id, numericality: { other_than: 1 }
+
+  with_options presence: true, format: { with:/\A[0-9]+\z/ } do
+    validates :price
+  end
+end
