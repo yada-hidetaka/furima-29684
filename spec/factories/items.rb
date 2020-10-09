90134dc16f69538258e7357809021e5f4a281792
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-10-09 18:27:17 +0900

    モデルテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'test' }
+    text { 'test' }
+    price { '1000' }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload("/files/sample1.png")
+  end
+
+  describe '出品機能' do
+    context '出品がうまくいく時' do
+      it "名前、画像、商品説明、カテゴリ、状態、配送料、発送元、日数、価格が存在すれば出品できる" do
+      expect(@item).to be_valid
+      end
+    end
+
+
+    context '出品がうまくいかない時' do
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank
+          ")
+      end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'textが空では登録できない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'categoryが空では登録できない' do
+        @item.category = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'ship_formが空では登録できない' do
+        @item.ship_form = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship form must be other than 1")
+      end
+      it 'prefectureが空では登録できない' do
+        @item.prefecture = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'statusが空では登録できない' do
+        @item.status = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it 'dayが空では登録できない' do
+        @item.day = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day must be other than 1")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceは半角数字のみである' do
+        @item.price = '１１１０'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceは300~9999999の範囲でなければならない' do
+        @item.price = '1'
+        @item.valid?
+        eexpect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+    end
