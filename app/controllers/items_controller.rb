c705169c3159733c4e67808931917bf94ce32b0d
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-10-10 22:08:24 +0900

    編集分岐と編集画面のコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: :show
+  before_action :set_item, only: [:show, :edit]
   def index 
     @items = Item.all.order("created_at DESC")
   end
@@ -19,6 +19,19 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
  
   private
