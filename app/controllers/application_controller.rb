d4c3b4ed89bf9608ab3dc2e20e0f1188ad8465f0
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-10-19 15:34:56 +0900

    修正1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -13,5 +12,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :birthday, :first_name_kana, :last_name_kana])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :encrypted_password])
   end
 end
